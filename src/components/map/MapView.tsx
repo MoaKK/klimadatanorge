353fa64e7dc0da64df579f3d72367701e5b834e0
Mohammad Kazem Khajeh <Kazem_kh95@hotmail.com>
2026-05-25 02:42:49 +0200

    feat: register map instance in store on load

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -5,6 +5,7 @@ import { useLocale } from "next-intl";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { mapLocales } from "@/utils/mapLocales";
+import { useMapStore } from "@/store/mapStore";
 
 const NORWAY_CENTER: [number, number] = [15, 65];
 const DEFAULT_ZOOM = 4;
@@ -13,6 +14,8 @@ const OFMAP_STYLE = "https://tiles.openfreemap.org/styles/liberty";
 function MapView() {
   const containerRef = useRef<HTMLDivElement>(null);
   const locale = useLocale();
+
+  const { setMap } = useMapStore();
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -25,10 +28,16 @@ function MapView() {
       locale: mapLocales[locale],
     });
 
-    return () => map.remove();
-  }, [locale]);
+    map.on("load", () => setMap(map));
 
-  return <div ref={containerRef} className="relative w-full h-full" />;
+    return () => {
+      setMap(null);
+      map.remove();
+    };
+  }, [locale, setMap]);
+
+
+  return <div ref={ containerRef } className="relative w-full h-full" />;
 }
 
 export { MapView };
