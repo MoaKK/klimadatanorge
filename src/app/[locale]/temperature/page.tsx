dec6a8424d2ffb48d0ea3f333c13ea33c67b25e3
Mohammad Kazem Khajeh <Kazem_kh95@hotmail.com>
2026-05-25 04:01:08 +0200

    feat: wire temperature page to TemperatureMode

--- src/app/[locale]/temperature/page.tsx
+++ src/app/[locale]/temperature/page.tsx
@@ -2,6 +2,8 @@ import { hasLocale } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
+import { temperatureData, MIN_YEAR, MAX_YEAR } from "@/data/temperature";
+import { TemperatureMode } from "@/components/modes/temperature/TemperatureMode";
 
 type Props = {
   params: Promise<{ locale: string }>;
@@ -15,7 +17,16 @@ async function Page({ params }: Props) {
   }
 
   setRequestLocale(locale);
-  return null;
+
+  return (
+    <TemperatureMode
+      data={temperatureData}
+      minYear={MIN_YEAR}
+      maxYear={MAX_YEAR}
+    />
+  );
 }
+
+export const revalidate = 86400;
 
 export default Page;
