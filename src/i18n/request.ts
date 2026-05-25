14be0879011dff9b23c4944e140870cc8cbc13b2
Mohammad Kazem Khajeh <Kazem_kh95@hotmail.com>
2026-05-25 02:44:36 +0200

    fix: use static message imports and fix locale type in request config

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,12 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
+import nbMessages from "../../messages/nb.json";
+import enMessages from "../../messages/en.json";
+
+const allMessages = {
+  nb: nbMessages,
+  en: enMessages,
+} as const;
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
@@ -8,8 +15,9 @@ export default getRequestConfig(async ({ requestLocale }) => {
     locale = routing.defaultLocale;
   }
 
+  const typedLocale = locale as (typeof routing.locales)[number];
   return {
-    locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    locale: typedLocale,
+    messages: allMessages[typedLocale],
   };
 });
